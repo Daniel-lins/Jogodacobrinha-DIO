a594c5fabebd5d8fe8e0c5767ca818a1f5e2adfd
Daniel Lins <dlins300@gmail.com>
2021-05-26 21:36:16 -0300

    .

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ function iniciarJogo() {
  for(i = 1; i < snake.length; i++){
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
             clearInterval(jogo);
-            alert('Game Over :(');
+            alert('Game Over, pressione o F5 para reniciar o jogo ');
         }
     }
             
